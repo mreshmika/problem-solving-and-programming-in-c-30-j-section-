2748027e6e9fee6e06f5d01612f65449b2ecb6c0
mreshmika <93912398+mreshmika@users.noreply.github.com>
2021-12-03 10:26:53 +0530

    Create addition of 3 numbers.c

---
+++ addition of 3 numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d;
+printf("enter value of a,b,c:");
+scanf("%d %d %d",&a,&b,&c);
+d=a+b+c;
+printf("addition of %d,%d and %d is %d",a,b,c,d);
+return 0;
+}
