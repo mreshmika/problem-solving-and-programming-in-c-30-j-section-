9901d658bf671ad84dd50b53ae6b55b499f97d6b
mreshmika <93912398+mreshmika@users.noreply.github.com>
2021-12-13 19:44:51 +0530

    Create division of two numbers.c

---
+++ division of two numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+   float a,b,c;
+   printf("enter the values of a and b:");
+   scanf("%f %f",&a,&b);
+   c=a/b;
+   printf("the value of %f divided by %f is %f",a,b,c);
+  return 0;
+}
